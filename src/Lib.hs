eaa6d53c7011dbf224680111a0d04b1caaeb0334
kayhide <kayhide@gmail.com>
2016-07-31 14:44:52 +0900

    update routing.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
 
 module Lib
     ( run
     ) where
 
-import Data.Monoid
 import Web.Spock.Safe
+import Data.Monoid
+import Data.Text (pack, Text)
+
+helloR :: Path '[Text]
+helloR = "hello" <//> var
+
+addR :: Path '[Int, Int]
+addR = "calc" <//> var <//> "+" <//> var
 
 run :: IO ()
 run = runSpock 8080 $ spockT id $ do
     get root $
-        text "Hello World!"
-    get ("hello" <//> var) $ \name ->
+        html $ "<a href='" <> renderRoute addR 313 3 <> "'>Calculate 313 + 3</a>"
+    get helloR $ \name ->
         text $ "Hello " <> name <> "!"
+    get addR $ \a b ->
+        text $ pack $ show (a + b)
